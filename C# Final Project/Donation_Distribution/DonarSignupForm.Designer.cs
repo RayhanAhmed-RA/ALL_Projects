03ac685d03e4c2de398cd5a1a6fce961658dc469
RayhanAhmed-RA <68095894+RayhanAhmed-RA@users.noreply.github.com>
2020-08-17 20:39:26 +0600

    Add files via upload

---
+++ C# Final Project/Donation_Distribution/DonarSignupForm.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Donation_Distribution
+{
+    partial class DonarSignupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Donar_name_label = new System.Windows.Forms.Label();
+            this.Donar_phn_num_label = new System.Windows.Forms.Label();
+            this.Donar_dob_label = new System.Windows.Forms.Label();
+            this.Donar_name_textbox = new System.Windows.Forms.TextBox();
+            this.Donar_phn_num_textbox = new System.Windows.Forms.TextBox();
+            this.Donar_hide_checkBox = new System.Windows.Forms.CheckBox();
+            this.Donar_address_textBox = new System.Windows.Forms.TextBox();
+            this.Donar_fake_name_textbox = new System.Windows.Forms.TextBox();
+            this.Donar_email_textbox = new System.Windows.Forms.TextBox();
+            this.Donar_address_label = new System.Windows.Forms.Label();
+            this.Donar_fake_name_label = new System.Windows.Forms.Label();
+            this.Donar_email_label = new System.Windows.Forms.Label();
+            this.Donar_occupation_textBox = new System.Windows.Forms.TextBox();
+            this.Donar_gender_label = new System.Windows.Forms.Label();
+            this.Donar_occupation_label = new System.Windows.Forms.Label();
+            this.Donar_yourself_label = new System.Windows.Forms.Label();
+            this.donar_yourself_rich_textBox = new System.Windows.Forms.RichTextBox();
+            this.Donar_dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.Donar_male_radioButton = new System.Windows.Forms.RadioButton();
+            this.Donar_female_radioButton = new System.Windows.Forms.RadioButton();
+            this.Donar_other_radioButton = new System.Windows.Forms.RadioButton();
+            this.Signup_confirm_button1 = new System.Windows.Forms.Button();
+            this.Back_to_signup_type_button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Donar_name_label
+            // 
+            this.Donar_name_label.AutoSize = true;
+            this.Donar_name_label.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_name_label.Location = new System.Drawing.Point(95, 66);
+            this.Donar_name_label.Name = "Donar_name_label";
+            this.Donar_name_label.Size = new System.Drawing.Size(75, 24);
+            this.Donar_name_label.TabIndex = 0;
+            this.Donar_name_label.Text = "Name :";
+            // 
+            // Donar_phn_num_label
+            // 
+            this.Donar_phn_num_label.AutoSize = true;
+            this.Donar_phn_num_label.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_phn_num_label.Location = new System.Drawing.Point(95, 135);
+            this.Donar_phn_num_label.Name = "Donar_phn_num_label";
+            this.Donar_phn_num_label.Size = new System.Drawing.Size(156, 24);
+            this.Donar_phn_num_label.TabIndex = 1;
+            this.Donar_phn_num_label.Text = "Phone Number :";
+            // 
+            // Donar_dob_label
+            // 
+            this.Donar_dob_label.AutoSize = true;
+            this.Donar_dob_label.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_dob_label.Location = new System.Drawing.Point(95, 202);
+            this.Donar_dob_label.Name = "Donar_dob_label";
+            this.Donar_dob_label.Size = new System.Drawing.Size(143, 24);
+            this.Donar_dob_label.TabIndex = 2;
+            this.Donar_dob_label.Text = "Date of Birth :";
+            // 
+            // Donar_name_textbox
+            // 
+            this.Donar_name_textbox.Location = new System.Drawing.Point(312, 60);
+            this.Donar_name_textbox.Multiline = true;
+            this.Donar_name_textbox.Name = "Donar_name_textbox";
+            this.Donar_name_textbox.Size = new System.Drawing.Size(339, 30);
+            this.Donar_name_textbox.TabIndex = 3;
+            // 
+            // Donar_phn_num_textbox
+            // 
+            this.Donar_phn_num_textbox.Location = new System.Drawing.Point(312, 129);
+            this.Donar_phn_num_textbox.Multiline = true;
+            this.Donar_phn_num_textbox.Name = "Donar_phn_num_textbox";
+            this.Donar_phn_num_textbox.Size = new System.Drawing.Size(339, 30);
+            this.Donar_phn_num_textbox.TabIndex = 4;
+            this.Donar_phn_num_textbox.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // Donar_hide_checkBox
+            // 
+            this.Donar_hide_checkBox.AutoSize = true;
+            this.Donar_hide_checkBox.Font = new System.Drawing.Font("Arial Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_hide_checkBox.Location = new System.Drawing.Point(731, 392);
+            this.Donar_hide_checkBox.Name = "Donar_hide_checkBox";
+            this.Donar_hide_checkBox.Size = new System.Drawing.Size(337, 32);
+            this.Donar_hide_checkBox.TabIndex = 20;
+            this.Donar_hide_checkBox.Text = "Do you want hide massage?";
+            this.Donar_hide_checkBox.UseVisualStyleBackColor = true;
+            // 
+            // Donar_address_textBox
+            // 
+            this.Donar_address_textBox.Location = new System.Drawing.Point(944, 66);
+            this.Donar_address_textBox.Multiline = true;
+            this.Donar_address_textBox.Name = "Donar_address_textBox";
+            this.Donar_address_textBox.Size = new System.Drawing.Size(339, 30);
+            this.Donar_address_textBox.TabIndex = 26;
+            // 
+            // Donar_fake_name_textbox
+            // 
+            this.Donar_fake_name_textbox.Location = new System.Drawing.Point(944, 209);
+            this.Donar_fake_name_textbox.Multiline = true;
+            this.Donar_fake_name_textbox.Name = "Donar_fake_name_textbox";
+            this.Donar_fake_name_textbox.Size = new System.Drawing.Size(339, 30);
+            this.Donar_fake_name_textbox.TabIndex = 25;
+            // 
+            // Donar_email_textbox
+            // 
+            this.Donar_email_textbox.Location = new System.Drawing.Point(312, 275);
+            this.Donar_email_textbox.Multiline = true;
+            this.Donar_email_textbox.Name = "Donar_email_textbox";
+            this.Donar_email_textbox.Size = new System.Drawing.Size(339, 30);
+            this.Donar_email_textbox.TabIndex = 24;
+            // 
+            // Donar_address_label
+            // 
+            this.Donar_address_label.AutoSize = true;
+            this.Donar_address_label.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_address_label.Location = new System.Drawing.Point(727, 66);
+            this.Donar_address_label.Name = "Donar_address_label";
+            this.Donar_address_label.Size = new System.Drawing.Size(96, 24);
+            this.Donar_address_label.TabIndex = 23;
+            this.Donar_address_label.Text = "Address :";
+            // 
+            // Donar_fake_name_label
+            // 
+            this.Donar_fake_name_label.AutoSize = true;
+            this.Donar_fake_name_label.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_fake_name_label.Location = new System.Drawing.Point(727, 215);
+            this.Donar_fake_name_label.Name = "Donar_fake_name_label";
+            this.Donar_fake_name_label.Size = new System.Drawing.Size(124, 24);
+            this.Donar_fake_name_label.TabIndex = 22;
+            this.Donar_fake_name_label.Text = "Fake Name :";
+            // 
+            // Donar_email_label
+            // 
+            this.Donar_email_label.AutoSize = true;
+            this.Donar_email_label.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_email_label.Location = new System.Drawing.Point(95, 281);
+            this.Donar_email_label.Name = "Donar_email_label";
+            this.Donar_email_label.Size = new System.Drawing.Size(74, 24);
+            this.Donar_email_label.TabIndex = 21;
+            this.Donar_email_label.Text = "Email :";
+            // 
+            // Donar_occupation_textBox
+            // 
+            this.Donar_occupation_textBox.Location = new System.Drawing.Point(944, 136);
+            this.Donar_occupation_textBox.Multiline = true;
+            this.Donar_occupation_textBox.Name = "Donar_occupation_textBox";
+            this.Donar_occupation_textBox.Size = new System.Drawing.Size(339, 30);
+            this.Donar_occupation_textBox.TabIndex = 30;
+            // 
+            // Donar_gender_label
+            // 
+            this.Donar_gender_label.AutoSize = true;
+            this.Donar_gender_label.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_gender_label.Location = new System.Drawing.Point(727, 278);
+            this.Donar_gender_label.Name = "Donar_gender_label";
+            this.Donar_gender_label.Size = new System.Drawing.Size(88, 24);
+            this.Donar_gender_label.TabIndex = 29;
+            this.Donar_gender_label.Text = "Gender :";
+            // 
+            // Donar_occupation_label
+            // 
+            this.Donar_occupation_label.AutoSize = true;
+            this.Donar_occupation_label.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_occupation_label.Location = new System.Drawing.Point(727, 142);
+            this.Donar_occupation_label.Name = "Donar_occupation_label";
+            this.Donar_occupation_label.Size = new System.Drawing.Size(127, 24);
+            this.Donar_occupation_label.TabIndex = 27;
+            this.Donar_occupation_label.Text = "Occupation :";
+            // 
+            // Donar_yourself_label
+            // 
+            this.Donar_yourself_label.AutoSize = true;
+            this.Donar_yourself_label.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Donar_yourself_label.Location = new System.Drawing.Point(95, 362);
+            this.Donar_yourself_label.Name = "Donar_yourself_label";
+            this.Donar_yourself_label.Size = new System.Drawing.Size(195, 24);
+            this.Donar_yourself_label.TabIndex = 33;
+            this.Donar_yourself_label.Text = "Tell about yourself :";
+            // 
+            // donar_yourself_rich_textBox
+            // 
+            this.donar_yourself_rich_textBox.Location = new System.Drawing.Point(312, 362);
+            this.donar_yourself_rich_textBox.Name = "donar_yourself_rich_textBox";
+            this.donar_yourself_rich_textBox.Size = new System.Drawing.Size(339, 96);
+            this.donar_yourself_rich_textBox.TabIndex = 34;
+            this.donar_yourself_rich_textBox.Text = "";
+            // 
+            // Donar_dateTimePicker
+            // 
+            this.Donar_dateTimePicker.Location = new System.Drawing.Point(312, 202);
+            this.Donar_dateTimePicker.Name = "Donar_dateTimePicker";
+            this.Donar_dateTimePicker.Size = new System.Drawing.Size(339, 22);
+            this.Donar_dateTimePicker.TabIndex = 36;
+            // 
+            // Donar_male_radioButton
+            // 
+            this.Donar_male_radioButton.AutoSize = true;
+            this.Donar_male_radioButton.Location = new System.Drawing.Point(944, 285);
+            this.Donar_male_radioButton.Name = "Donar_male_radioButton";
+            this.Donar_male_radioButton.Size = new System.Drawing.Size(59, 21);
+            this.Donar_male_radioButton.TabIndex = 37;
+            this.Donar_male_radioButton.TabStop = true;
+            this.Donar_male_radioButton.Text = "Male";
+            this.Donar_male_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // Donar_female_radioButton
+            // 
+            this.Donar_female_radioButton.AutoSize = true;
+            this.Donar_female_radioButton.Location = new System.Drawing.Point(1079, 282);
+            this.Donar_female_radioButton.Name = "Donar_female_radioButton";
+            this.Donar_female_radioButton.Size = new System.Drawing.Size(75, 21);
+            this.Donar_female_radioButton.TabIndex = 38;
+            this.Donar_female_radioButton.TabStop = true;
+            this.Donar_female_radioButton.Text = "Female";
+            this.Donar_female_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // Donar_other_radioButton
+            // 
+            this.Donar_other_radioButton.AutoSize = true;
+            this.Donar_other_radioButton.Location = new System.Drawing.Point(1211, 285);
+            this.Donar_other_radioButton.Name = "Donar_other_radioButton";
+            this.Donar_other_radioButton.Size = new System.Drawing.Size(72, 21);
+            this.Donar_other_radioButton.TabIndex = 39;
+            this.Donar_other_radioButton.TabStop = true;
+            this.Donar_other_radioButton.Text = "Others";
+            this.Donar_other_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // Signup_confirm_button1
+            // 
+            this.Signup_confirm_button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Signup_confirm_button1.Location = new System.Drawing.Point(1032, 496);
+            this.Signup_confirm_button1.Name = "Signup_confirm_button1";
+            this.Signup_confirm_button1.Size = new System.Drawing.Size(251, 52);
+            this.Signup_confirm_button1.TabIndex = 40;
+            this.Signup_confirm_button1.Text = "Confirm";
+            this.Signup_confirm_button1.UseVisualStyleBackColor = true;
+            // 
+            // Back_to_signup_type_button1
+            // 
+            this.Back_to_signup_type_button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Back_to_signup_type_button1.Location = new System.Drawing.Point(828, 496);
+            this.Back_to_signup_type_button1.Name = "Back_to_signup_type_button1";
+            this.Back_to_signup_type_button1.Size = new System.Drawing.Size(150, 52);
+            this.Back_to_signup_type_button1.TabIndex = 41;
+            this.Back_to_signup_type_button1.Text = "Back";
+            this.Back_to_signup_type_button1.UseVisualStyleBackColor = true;
+            this.Back_to_signup_type_button1.Click += new System.EventHandler(this.Back_to_signup_type_button1_Click);
+            // 
+            // DonarSignupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1368, 599);
+            this.Controls.Add(this.Back_to_signup_type_button1);
+            this.Controls.Add(this.Signup_confirm_button1);
+            this.Controls.Add(this.Donar_other_radioButton);
+            this.Controls.Add(this.Donar_female_radioButton);
+            this.Controls.Add(this.Donar_male_radioButton);
+            this.Controls.Add(this.Donar_dateTimePicker);
+            this.Controls.Add(this.donar_yourself_rich_textBox);
+            this.Controls.Add(this.Donar_yourself_label);
+            this.Controls.Add(this.Donar_occupation_textBox);
+            this.Controls.Add(this.Donar_gender_label);
+            this.Controls.Add(this.Donar_occupation_label);
+            this.Controls.Add(this.Donar_address_textBox);
+            this.Controls.Add(this.Donar_fake_name_textbox);
+            this.Controls.Add(this.Donar_email_textbox);
+            this.Controls.Add(this.Donar_address_label);
+            this.Controls.Add(this.Donar_fake_name_label);
+            this.Controls.Add(this.Donar_email_label);
+            this.Controls.Add(this.Donar_hide_checkBox);
+            this.Controls.Add(this.Donar_phn_num_textbox);
+            this.Controls.Add(this.Donar_name_textbox);
+            this.Controls.Add(this.Donar_dob_label);
+            this.Controls.Add(this.Donar_phn_num_label);
+            this.Controls.Add(this.Donar_name_label);
+            this.Name = "DonarSignupForm";
+            this.Text = "DonarSignupForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Donar_name_label;
+        private System.Windows.Forms.Label Donar_phn_num_label;
+        private System.Windows.Forms.Label Donar_dob_label;
+        private System.Windows.Forms.TextBox Donar_name_textbox;
+        private System.Windows.Forms.TextBox Donar_phn_num_textbox;
+        private System.Windows.Forms.CheckBox Donar_hide_checkBox;
+        private System.Windows.Forms.TextBox Donar_address_textBox;
+        private System.Windows.Forms.TextBox Donar_fake_name_textbox;
+        private System.Windows.Forms.TextBox Donar_email_textbox;
+        private System.Windows.Forms.Label Donar_address_label;
+        private System.Windows.Forms.Label Donar_fake_name_label;
+        private System.Windows.Forms.Label Donar_email_label;
+        private System.Windows.Forms.TextBox Donar_occupation_textBox;
+        private System.Windows.Forms.Label Donar_gender_label;
+        private System.Windows.Forms.Label Donar_occupation_label;
+        private System.Windows.Forms.Label Donar_yourself_label;
+        private System.Windows.Forms.RichTextBox donar_yourself_rich_textBox;
+        private System.Windows.Forms.DateTimePicker Donar_dateTimePicker;
+        private System.Windows.Forms.RadioButton Donar_male_radioButton;
+        private System.Windows.Forms.RadioButton Donar_female_radioButton;
+        private System.Windows.Forms.RadioButton Donar_other_radioButton;
+        private System.Windows.Forms.Button Signup_confirm_button1;
+        private System.Windows.Forms.Button Back_to_signup_type_button1;
+    }
+}
---
+++ C# Final Project/Donation_Distribution/DonarSignupForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Donation_Distribution
+{
+    public partial class DonarSignupForm : Form
+    {
+        public DonarSignupForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Back_to_signup_type_button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f2 = new Form1();
+            f2.Show();
+        }
+    }
+}
